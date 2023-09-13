8b0ad2629a9d50afd1e9f4e07ab9e50add72971b
FranSanabri <death.the.system@gmail.com>
2023-09-13 14:13:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'; // Asegúrate de importar el componente Navigate
 import Navbar from './components/Navbar/Navbar';
 import Store from './components/Store/Store';
 import PasateAClaro from './components/PasateAClaro/PasateAClaro';
 import PackYa from './components/PackYa/PackYa';
 import Ayuda from './components/Ayuda/Ayuda';
-import Servicios from './components/Servicios/Servicios'; // Importa el nuevo componente "Servicios"
-import './App.css';
+import Servicios from './components/Servicios/Servicios';
 
 function App() {
   return (
@@ -17,7 +16,9 @@ function App() {
         <Route path="/pasate-a-claro" element={<PasateAClaro />} />
         <Route path="/pack-ya" element={<PackYa />} />
         <Route path="/ayuda" element={<Ayuda />} />
-        <Route path="/servicios" element={<Servicios />} /> {/* Nueva ruta para "Servicios" */}
+        <Route path="/servicios" element={<Servicios />} />
+        {/* Agrega una redirección desde la ruta raíz ("/") a "/servicios" */}
+        <Route path="/" element={<Navigate to="/servicios" />} />
       </Routes>
     </div>
   );
